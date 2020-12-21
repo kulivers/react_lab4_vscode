7182cfb6d3c22eb0caf77b92c0178653da6635dc
kulivers <kulivers@mail.ru>
2020-12-21 18:41:23 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileContent: 'empty',
+      fileName: 'ss'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Создание и удаление файла</h1>
+          <input type="text" name="fileName" value={this.state.fileName} />
+          .txt
+          <br></br>
+          <button name="open">Open</button>
+          <button name="delete">Delete</button>
+          <button name="edit">Edit</button>
+        </div>
+        <br />
+        <br />
+        <div>
+          <h1>Изменение содержимого файла</h1>
+          <textarea
+            name="fileContent"
+            value={this.state.fileContent}
+            cols={100}
+            rows={15}
+          />
+        </div>
+        <Button name="editContent">Edit</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
